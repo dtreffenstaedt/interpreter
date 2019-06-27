12f24c07932d15c7336f59f3c265d9e3246dba20
Daniel Treffenstädt <d.treffenstaedt@gmail.com>
2019-06-27 12:11:50 +0200

    added assignment operations and scopes to parser

--- include/Parser.h
+++ include/Parser.h
@@ -17,12 +17,12 @@ class UnexpectedToken : public std::exception
 {
 private:
     Token token;
-    Token::Type type;
+    Token expected;
 
 public:
-    UnexpectedToken(Token to, Token::Type ty) :
+    UnexpectedToken(Token to, Token exp) :
         token(to),
-        type(ty)
+        expected(exp)
     {}
 
     void print()
@@ -30,6 +30,9 @@ public:
         std::wstring buf;
         buf = L"Unexpected Token: ";
         buf += token.name();
+        buf += L" (expected:";
+        buf += expected.name();
+        buf += L")";
         buf += L"\n\tat ";
         buf += std::to_wstring(token.pos().row);
         buf += L":";
@@ -94,21 +97,104 @@ private:
             nextToken();
             return;
         }
-        throw UnexpectedToken((*m_currentToken), tokenType);
+        throw UnexpectedToken((*m_currentToken), Token({1,1}, tokenType));
+    }
+
+    inline std::shared_ptr<AST> compountStatement()
+    {
+        eat(Token::Type::LBrace);
+        std::shared_ptr<Compount> root = std::make_shared<Compount>(Compount());
+        root->append(statement());
+        while ((*m_currentToken) == Token::Type::Semicolon)
+        {
+            eat(Token::Type::Semicolon);
+            if ((*m_currentToken) == Token::Type::RBrace)
+            {
+                break;
+            }
+            root->append(statement());
+        }
+        eat(Token::Type::RBrace);
+        return root;
+    }
+
+    inline std::shared_ptr<AST> statement()
+    {
+        if ((*m_currentToken) == Token::Type::LBrace)
+        {
+            return compountStatement();
+        }
+        else if ((*m_currentToken) == Token::Type::Literal)
+        {
+            return assignment();
+        }
+        return std::make_shared<Empty>(Empty());
+    }
+
+    inline std::shared_ptr<AST> variable()
+    {
+       std::shared_ptr<AST> var = std::make_shared<Variable>(Variable(m_currentToken));
+       eat(Token::Type::Literal);
+       return var;
+    }
+
+    inline std::shared_ptr<AST> assignment()
+    {
+        std::shared_ptr<AST> var = variable();
+        std::shared_ptr<Token> t = m_currentToken;
+        if ((*t) == Token::Type::OperatorAssign)
+        {
+            eat(Token::Type::OperatorAssign);
+        }
+        else if ((*t) == Token::Type::OperatorAddAssign)
+        {
+            eat(Token::Type::OperatorAddAssign);
+        }
+        else if ((*t) == Token::Type::OperatorSubAssign)
+        {
+            eat(Token::Type::OperatorSubAssign);
+        }
+        else if ((*t) == Token::Type::OperatorMultAssign)
+        {
+            eat(Token::Type::OperatorMultAssign);
+        }
+        else if ((*t) == Token::Type::OperatorDivAssign)
+        {
+            eat(Token::Type::OperatorDivAssign);
+        }
+        return std::make_shared<Assignment>(Assignment(var,t,expression()));
+        std::shared_ptr<AST> right = variable();
     }
 
     inline std::shared_ptr<AST> factor()
     {
         std::shared_ptr<Token> f = m_currentToken;
-        if ((*f) == Token::Type::LParen)
+
+        if ((*f) == Token::Type::Number)
+        {
+            eat(Token::Type::Number);
+            return std::make_shared<Number>(Number(f));
+        }
+        else if ((*f) == Token::Type::OperatorPlus)
+        {
+            eat(Token::Type::OperatorPlus);
+            std::shared_ptr<AST> result = std::make_shared<UnaryOperation>(UnaryOperation(f, factor()));
+            return result;
+        }
+        if ((*f) == Token::Type::OperatorMinus)
+        {
+            eat(Token::Type::OperatorMinus);
+            std::shared_ptr<AST> result = std::make_shared<UnaryOperation>(UnaryOperation(f, factor()));
+            return result;
+        }
+        else if ((*f) == Token::Type::LParen)
         {
             eat(Token::Type::LParen);
             std::shared_ptr<AST> result = expression();
             eat(Token::Type::RParen);
             return result;
         }
-        eat(Token::Type::Number);
-        return std::make_shared<Number>(Number(f));
+        return variable();
     }
 
     inline std::shared_ptr<AST> exponent()
@@ -176,7 +262,7 @@ public:
 
     std::shared_ptr<AST> parse()
     {
-        std::shared_ptr<AST> result = expression();
+        std::shared_ptr<AST> result = statement();
         eat(Token::Type::End);
         return result;
     }
